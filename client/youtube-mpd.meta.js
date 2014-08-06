08bcb4f86cc6f3e26e2eda562e6e18fec3432341
Jan Hruban <jan.hruban@gooddata.com>
2014-08-06 14:36:16 +0200

    Use update and download URLs

---
+++ client/youtube-mpd.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        youtube-mpd
+// @namespace   hrubi@inkorekt.cz
+// @description Forwards youtube link to another site
+// @include     /https?://(.*\.)?youtube.com/.*/
+// @version     0.1
+// @downloadURL https://raw.githubusercontent.com/hrubi/ytmpd/master/client/youtube-mpd.user.js
+// @updateURL   https://raw.githubusercontent.com/hrubi/ytmpd/master/client/youtube-mpd.meta.js
+// @grant       GM_xmlhttpRequest
+// @require     https://raw.githubusercontent.com/hrubi/ytmpd/master/client/jquery.min.js
+// @require     https://raw.githubusercontent.com/hrubi/ytmpd/master/client/jquery.noty.packaged.min.js
